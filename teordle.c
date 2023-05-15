16227e0a533b48e5f41fbe6716804b9fd0a6c72e
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-05-15 05:00:24 +0200

    nearly full rewrite
    code is better now, more macros and gotos :)

--- teordle.c
+++ teordle.c
@@ -1,63 +1,112 @@
 #include <assert.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+#define clear_screen() system("clear")
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+#define clear_screen() system("cls")
+#endif
+
 #define COLOR_GREEN "\x1B[32m"
 #define COLOR_YELLOW "\x1B[33m"
 #define COLOR_WHITE "\x1B[37m"
+
+#define LETTER_SEPERATOR_STR "_ "
 #define NULL_CHAR '\0'
+#define NEWLINE_STR "\n"
+#define NEWLINE_CHAR (char) '\n'
+
+#define WORD_LENGTH 5
+#define MAX_GUESSES 6
+#define LAST_CHARACTER WORD_LENGTH
+#define GUESS_ARRAY_SIZE MAX_GUESSES * WORD_LENGTH
+
+#define NOT_IN_WORD 0
+#define IN_WORD_WRONG_INDEX 1
+#define IN_WORD_CORRECT_INDEX 2
+
+#define PLAYER_WON true
+#define PLAYER_LOST false
 
 #define assert_print(condition, string) \
     if (!(condition)) {                 \
         printf("%s", string);           \
-        exit(1);                        \
+        terminate(1);                   \
     }
 
 
 void get_input();
+void terminate(uint8_t exit_code);
 
 FILE* file_ptr;
 
-int words_length = 0;
-int current_line = 0;
+size_t words_length = 0;
+size_t current_line = 0;
 
-char current_word[6];
-char user_input[6];
-char guess_list[30];
+unsigned char user_input[WORD_LENGTH + 1];
+unsigned char current_word[WORD_LENGTH + 1];
+unsigned char guess_list[GUESS_ARRAY_SIZE];
 
 bool game_over = false;
 
 
-bool
-contains_character(
-    char ch)
+uint8_t
+check_letter_in_word(
+    unsigned char letter,
+    size_t guess_list_index)
 {
-    for (int i = 0; i < 5; ++i) 
-        if (current_word[i] == ch)
-            return(true);
+    if (current_word[guess_list_index] == letter)
+        return IN_WORD_CORRECT_INDEX;
 
-    return(false);
+    for (uint8_t i = 0; i < WORD_LENGTH; ++i) 
+        if (current_word[i] == letter)
+            return IN_WORD_WRONG_INDEX;
+
+    return NOT_IN_WORD;
 
 }
 
 
+unsigned char*
+determine_letter_color(
+    unsigned char letter,
+    size_t guess_list_index)
+{
+    switch (check_letter_in_word(
+        letter, guess_list_index))
+    {
+    case IN_WORD_CORRECT_INDEX:
+        return COLOR_GREEN;
+    case IN_WORD_WRONG_INDEX:
+        return COLOR_YELLOW;
+    default:
+        return COLOR_WHITE;  
+    }
+}
+
+
 void
-print_char(
-    char ch ,
+print_letter(
+    unsigned char letter,
     char* color)
 {
-    printf("%s%c%s " , color, ch, COLOR_WHITE);
+    printf("%s%c%s ", color, letter, COLOR_WHITE);
 }
 
 
 void
-get_word()
+assign_random_word()
 {
-    char ch;
+start_label:
+    unsigned char ch;
 
     if (!file_ptr) {
         file_ptr = fopen("words.txt" , "r");
@@ -65,123 +114,125 @@ get_word()
         // prints a message and exits if file_ptr is NULL;
         assert_print(file_ptr, "Unable to find or open words.txt");
 
-        // Get length of word list
+        // get length of word list
         fseek(file_ptr, 0, SEEK_END);
-        words_length = ftell(file_ptr) - 6; // subtract to adjust for the last word
+        words_length = ftell(file_ptr) - WORD_LENGTH - 1; // subtract to adjust for the last word
     }
 
     // really should use a better random function, barely ever see
     // any of the words in the last half of the word list
     fseek(file_ptr, rand() % words_length, SEEK_SET);
 
-    // Get a word from the list
+    // get a word from the list
     while (ch = fgetc(file_ptr)) {
-        if (ch == '\n') {
-            for (int i = 0; i < 5; ++i)
+        if (ch == NEWLINE_CHAR) {
+            for (size_t i = 0; i < WORD_LENGTH; ++i)
                 current_word[i] = toupper(fgetc(file_ptr));
             return;
         }
     }
 
     // EOF reached, try again
-    get_word();
-}
-
-
-void
-clear()
-{
-    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
-    system("clear");
-    #endif
-
-    #if defined(_WIN32) || defined(_WIN64)
-    system("cls");
-    #endif
+    goto start_label;
 }
 
 
 void
 string_toupper(
-    char* str)
+    unsigned char* str)
 {
-    int i = 0;
-    while (str[i]) {
-        str[i] = toupper(str[i]);
-        ++i;
-    }
+    while (*str)
+        *(str++) = toupper(*str);
 }
 
 
 bool
 is_input_valid(
-    char* str)
+    unsigned char* str)
 {
-    for (int i = 0; i < 5; ++i) {
-        if (!str[i])
-            return(false);
-    }
+    // check the validity of the first 5
+    for (int i = 0; i < WORD_LENGTH; ++i)
+        if (!str[i] || !isalpha(str[i])) // yay ascii only!
+            return false;
 
-    return(!str[5]);
+    // forcing NULL at input[5] just made things annoying
+    // when you accidentally type one letter too many
+    // or your brain fails to tell you that a certain word has
+    // six letters and not five, and it gets treated as a
+    // valid input because it was terminated after five letters.
+    return !str[LAST_CHARACTER];
 }
 
 
 void
 render()
 {
-    clear();
-    int curr = 0;
-    for (int i = 0; i < 30; ++i) {
-        if (!guess_list[i])
-            printf("_ ");
-        else {
-            if (guess_list[i] == current_word[curr]) {
-                print_char(guess_list[i] , COLOR_GREEN);
-            } else {
-                if (contains_character(guess_list[i])) {
-                    print_char(guess_list[i] , COLOR_YELLOW);
-                } else
-                    print_char(guess_list[i] , COLOR_WHITE);
-            }
-        }
+    clear_screen();
+    
+    unsigned char* color;
 
-        if ((i + 1) % 5 == 0)
-            printf("\n");
+    for (size_t i = 0; i < GUESS_ARRAY_SIZE; ++i) {
+        size_t letter_guess_array_index = i % WORD_LENGTH;
 
-        ++curr;
-        if (curr >= 5)
-            curr = 0;
+        if (letter_guess_array_index == 0 && i != 0)
+            printf(NEWLINE_STR);
+
+        if (guess_list[i])
+            print_letter(
+                guess_list[i], 
+                determine_letter_color(guess_list[i], letter_guess_array_index));
+        else
+            printf(LETTER_SEPERATOR_STR);
     }
+
+    printf(NEWLINE_STR);
+}
+
+
+void 
+end_game(
+    bool victory)
+{
+    game_over = true;
+
+    if (victory)
+        printf("\nYou win!\n\n");
+    else
+        printf("\nYou lose, the word was: %s\n\n", current_word);
+
+    printf("PLAY | EXIT\n");
 }
 
 
 void
 check_victory()
 {
-    bool guessed_right = strcmp(user_input , current_word) == 0;
+
+/*
+ * I'm not into the extra indentation level for a while (true) loop
+ * so we're getting jiggy with the goto up in here.
+ */
+
+start_label:
+    bool guessed_correct_word = strncmp(
+        user_input, current_word, WORD_LENGTH) == 0;
+
     render();
 
-    if (current_line > 5 && !guessed_right) {
-        printf("\nYou lose, the word was: %s \n\n" , current_word);
-        printf("PLAY | EXIT\n");
-        get_input();
-    }
+    if (guessed_correct_word)
+        end_game(PLAYER_WON);
+    if (current_line == MAX_GUESSES && !guessed_correct_word)
+        end_game(PLAYER_LOST);
 
-    if (guessed_right) {
-        printf("\nYou win!\n\n");
-        printf("PLAY | EXIT\n");
-        get_input();
-    } else {
-        get_input();
-        check_victory();
-    }
+    get_input();
+    goto start_label;
 }
 
 
 void
 clear_guess_list()
 {
-    for (int i = 0; i < 30; ++i)
+    for (size_t i = 0; i < GUESS_ARRAY_SIZE; ++i)
         guess_list[i] = NULL_CHAR;
 }
 
@@ -189,39 +240,37 @@ clear_guess_list()
 int
 start_game()
 {
-    // Current attemps
+    // current attempt number
     current_line = 0;
+    game_over = false;
 
-    clear();
+    clear_screen();
     clear_guess_list();
-
-    get_word();
+    assign_random_word();
     check_victory();
-
-    return(0);
 }
 
 
 void
 get_input()
 {
-    char str[7];
+    unsigned char guess[7];
 
-    if (fgets(str , sizeof(str) , stdin)) {
-        str[strcspn(str , "\n")] = '\0';
-        string_toupper(str);
+    if (fgets(guess , WORD_LENGTH + 2 , stdin)) {
+        guess[strcspn(guess, NEWLINE_STR)] = NULL_CHAR;
+        string_toupper(guess);
     }
 
-    if (strlen(str) > 5) 
+    if (strlen(guess) > WORD_LENGTH) 
         get_input();
 
-    if (strcmp(str , "PLAY") == 0) start_game();
-    if (strcmp(str , "EXIT") == 0) exit(0);
+    if (strcmp(guess , "PLAY") == 0) start_game();
+    if (strcmp(guess , "EXIT") == 0) terminate(0);
 
-    if (is_input_valid(str)) {
-        for (int i = 0; i < 5; ++i) {
-            user_input[i] = str[i];
-            guess_list[(5 * current_line) + i] = str[i];
+    if (is_input_valid(guess) && !game_over) {
+        for (int i = 0; i < WORD_LENGTH; ++i) {
+            user_input[i] = guess[i];
+            guess_list[(5 * current_line) + i] = guess[i];
         }
         ++current_line;
     }
@@ -233,13 +282,23 @@ main()
 {
     srand(time(NULL));
     
-    while (true) {
-        printf("Teordle - Wordle, but in the terminal\n");
-        printf("---------------------------------------\n");
-        printf("PLAY | EXIT\n");
-        get_input();
-        clear();
-    }
+    printf("Teordle - Wordle, but in the terminal\n");
+    printf("---------------------------------------\n");
+    printf("PLAY | EXIT\n");
+    get_input();
+    clear_screen();
 
-    return(0);
+    terminate(0);
+}
+
+
+
+void 
+terminate(
+    uint8_t exit_code)
+{
+    if (file_ptr)
+        fclose(file_ptr);
+
+    exit(exit_code);
 }
